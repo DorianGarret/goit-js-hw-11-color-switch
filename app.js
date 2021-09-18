0ace3327e82d67b4ee4a21afe155ec64f1f05f27
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-09-18 21:50:23 +0300

    Добавил настройки prettier

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const refs = {
-  btn1: document.querySelector('[data-action="start"]'),
+    start: document.querySelector('[data-action="start"]'),
+    stop: document.querySelector('[data-action="stop"]'),
 };
-refs.btn1.addEventListener('click', () => {
-  console.log(`object`);
-});
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
