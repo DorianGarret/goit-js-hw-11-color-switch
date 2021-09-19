d1a5c331e72a29058f602840b53512e25b9d0193
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-09-19 06:22:31 +0300

    Добавил функционал отключения и включения кнопок

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const refs = {
-    start: document.querySelector('[data-action="start"]'),
-    stop: document.querySelector('[data-action="stop"]'),
+    btnStart: document.querySelector('[data-action="start"]'),
+    btnStop: document.querySelector('[data-action="stop"]'),
 };
+const { btnStart, btnStop } = refs;
 
 const colors = [
     '#FFFFFF',
@@ -12,6 +13,23 @@ const colors = [
     '#795548',
 ];
 
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', checkDisabledBtn);
+btnStop.addEventListener('click', checkDisabledBtn);
+
+function checkDisabledBtn(event) {
+    if (event.target.dataset.action === 'start') {
+        btnStop.disabled = false;
+        btnStart.disabled = true;
+        return;
+    }
+    if (event.target.dataset.action === 'stop') {
+        btnStop.disabled = true;
+        btnStart.disabled = false;
+        return;
+    }
+}
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
